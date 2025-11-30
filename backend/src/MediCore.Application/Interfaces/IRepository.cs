8fbe01fccd30627c5cac328d2a9d33bf1bac4641
vaibhaviimcal-web <vaibhav.iimcal@gmail.com>
2025-12-01 03:59:21 +0530

    Add generic repository interface

---
+++ backend/src/MediCore.Application/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+using System.Linq.Expressions;
+using MediCore.Domain.Entities;
+
+namespace MediCore.Application.Interfaces;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+}
